a4426cf10ab0076a43075de9e0f8aeb5dc7207f2
Ploypaphat Saltz <ploypaphat.saltz@gmail.com>
2025-08-18 19:21:48 -0700

    [docs]: remove unwanted comment

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,6 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    # app.config['JWT_SECRET_KEY'] = 'secret_key'
     app.config.from_object(Config)
     
     jwt.init_app(app)
